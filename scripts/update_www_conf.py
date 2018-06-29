9a1848ebb6895e3ede210869d4188e0172002995
brian978 <andrei.dincescu1@gmail.com>
2018-06-29 10:59:27 +0300

    Updated variable name

--- scripts/update_www_conf.py
+++ scripts/update_www_conf.py
@@ -3,7 +3,7 @@ import os
 import socket
 import fileinput
 
-dir = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + "/../")
+parent_dir = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + "/../")
 
 # IP detect
 socket_obj = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -11,14 +11,14 @@ socket_obj.connect(("8.8.8.8", 80))
 ip = socket_obj.getsockname()[0]
 
 # Update the PHP-FPM file
-for line in fileinput.input(dir + "/container-config/fpm/www.conf", inplace=True):
+for line in fileinput.input(parent_dir + "/container-config/fpm/www.conf", inplace=True):
     if line.find("php_admin_value[xdebug.remote_host]") != -1:
         print("php_admin_value[xdebug.remote_host] = " + ip)
     else:
         sys.stdout.write(line)
 
 # Update the Docker compose file
-for line in fileinput.input(dir + "/docker-compose.yml", inplace=True):
+for line in fileinput.input(parent_dir + "/docker-compose.yml", inplace=True):
     if line.find("ADV_HOST") != -1:
         print("      ADV_HOST: " + ip)
     else:
